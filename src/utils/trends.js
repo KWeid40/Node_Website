516be42820a6b72cd22d559704b97e8cbe527828
Kyle Weidenmann <kweidenmann40@gmail.com>
2023-03-03 10:31:21 +0200

    require fix

--- src/utils/trends.js
+++ src/utils/trends.js
@@ -1,5 +1,5 @@
 const googleTrends = require("google-trends-api");
-import * as countries from("./data/countries.json");
+const countries = require("/data/countries.json");
 
 const getCountries = async (callback) => {
   if (countries) {
